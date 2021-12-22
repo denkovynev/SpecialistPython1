38cc2e04156175f204709e1353c7000aedea34db
denkovynev <58596650+denkovynev@users.noreply.github.com>
2021-12-22 18:30:35 +0300

    Update 02_hw.py

--- Module3/home_work/02_hw.py
+++ Module3/home_work/02_hw.py
@@ -6,3 +6,11 @@ import random
 numbers = []
 # print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
 # TODO: your code here
+n = int(input("Введите n "))
+numbers = []
+i = 0
+while i < n:
+    i += 1
+    numbers.append(random.randint(-100, 100))
+
+print(numbers)
