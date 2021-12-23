bdb641b3193eac079df6cbe025f5008ad323d290
denkovynev <58596650+denkovynev@users.noreply.github.com>
2021-12-23 09:11:24 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,10 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+max_len_names = len(names[0])
+max_names = ""
+for name in names:
+    if len(name) > max_len_names:
+        max_len_names = len(name)
+        max_names = name
+print(max_names)
